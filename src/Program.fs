dfeb6fe97fdbe0f03e1ac33131cdfd2314231ba4
Jon Roberts <jon.roberts@agilecraft.com>
2017-09-17 18:45:43 -0500

    remove assemblyinfo and package template.

--- src/Program.fs
+++ src/Program.fs
@@ -1,9 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open Hello.World.Say
+open Hello.World
 
 [<EntryPoint>]
 let main argv =
-    hello "World from F#!"
+    Say.hello "World from F#!"
     0 // return an integer exit code
